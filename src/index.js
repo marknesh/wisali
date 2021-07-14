4d6c904544606add481c8ae6715b60bccc0caacf
nesh <markmunene.nesh@gmail.com>
2021-07-14 21:45:32 +0300

    import static files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './components/Navbar';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <Navbar />
+  </React.StrictMode>,
+
+  document.getElementById('wrapper')
+);
 ReactDOM.render(
   <React.StrictMode>
     <App />

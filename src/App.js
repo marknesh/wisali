0dd73bda3e1df29e72d468a46f171a6c20c6550c
nesh <markmunene.nesh@gmail.com>
2021-07-15 17:38:34 +0300

    show counterup

--- src/App.js
+++ src/App.js
@@ -6,26 +6,12 @@ function App() {
     <div>
          <Helmet>
          
-           
-       
-<script src="/scripts/chosen.min.js"></script> 
-<script src="/scripts/magnific-popup.min.js"></script> 
-<script src="/scripts/owl.carousel.min.js"></script> 
-<script src="/scripts/rangeSlider.js"></script> 
-<script src="/scripts/sticky-kit.min.js"></script> 
-<script src="/scripts/slick.min.js"></script> 
-<script src="/scripts/mmenu.min.js"></script> 
-<script src="/scripts/tooltips.min.js"></script> 
-<script src="/scripts/masonry.min.js"></script> 
+ 
+
 <script src="/scripts/jquery.counterup.min.js"></script> 
 
 
 
-
-<script src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script> 
-<script src="/scripts/infobox.min.js"></script> 
-<script src="/scripts/markerclusterer.js"></script> 
-<script src="/scripts/maps.js"></script>
             </Helmet>
         
 

02b9a8756f1d68bbdcd107417788d55dba72be03
nesh <markmunene.nesh@gmail.com>
2021-07-30 16:25:44 +0300

    deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -448,11 +448,10 @@ signOutRef = (element) => {
   
 
 
-<script src={`http://maps.google.com/maps/api/js?sensor=false&amp;language=en`}></script> 
-
+           <script src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script>  
 <script src="/scripts/infobox.min.js"></script> 
 <script src="/scripts/markerclusterer.js"></script> 
-<script src="/scripts/maps.js"></script>
+<script src="/scripts/maps.js"></script> 
             </Helmet>
         
           
